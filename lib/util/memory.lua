a454d895360ddfaf518c7334b83061d170f3178c
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-09-02 00:33:10 +0300

    fix: add TB in memory

--- lib/util/memory.lua
+++ lib/util/memory.lua
@@ -4,16 +4,21 @@
 ---@param kb number
 ---@return string
 local function format(kb)
-	local unit = "Kb"
+	local unit = "KB"
 	local n = kb
 	if kb >= 1024 then -- 2 ^ 10
 		n = n / 1024
-		unit = "mb"
+		unit = "MB"
 	end
 
-	if kb >= 1048576 then -- 2 ^ 20
+	if kb >= 1048576 then -- 2 ^ 30
 		n = n / 1048576
-		unit = "gb"
+		unit = "GB"
+	end
+
+	if kb >= 1073741824 then -- 2 ^ 30
+		n = n / 1073741824
+		unit = "TB"
 	end
 
 	return string.format("%." .. 2 .. "f%s", n, unit)
