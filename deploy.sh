db1df4a60474eea17bcebaf7a3900d69090cd597
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-10-04 19:19:39 +0300

    feat: prepare deployment

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+API_KEY=$(cat api_key)
+VERSION="$1"
+
+if [[ -z "$VERSION" ]]; then
+  echo "No version set as \$1 arg"
+  exit 1
+fi
+
+# echo "Pushing to git"
+# git tag "$VERSION"
+# git push --tags
+# git push origin "$VERSION"
+
+specfile="laura-$VERSION.rockspec"
+echo "Packing rock..."
+luarocks pack "$specfile"
+
+echo "Uploading rock..."
+luarocks upload "$specfile" --api-key="$API_KEY"
+
+echo "Deployed $VERSION"
