f13d365432378718c8ab350eefd29b313c303f1c
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-10-13 02:19:28 +0300

    release: ./src/laura/v.lua

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 API_KEY=$(cat api_key)
 VERSION="$1"
 GIT_BRANCH="$VERSION"
+VERSION_FILE="./src/laura/v.lua"
 
 if [[ -z "$VERSION" ]]; then
   VERSION="dev-0"
@@ -9,6 +10,10 @@ fi
 
 specfile="laura-$VERSION.rockspec"
 rockfile="laura-$VERSION.src.rock"
+
+printf "return \"%s\"" "$VERSION" > "$VERSION_FILE"
+git add "$VERSION_FILE"
+git ci -m "release: $VERSION_FILE"
 
 echo "Pushing to git..."
 if [ "$VERSION" != "dev-0" ]; then
