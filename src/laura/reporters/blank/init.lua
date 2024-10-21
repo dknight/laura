d031178a44fca7a8c279600771477ea52ee0cdb9
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-10-21 21:54:05 +0300

    refactor: rename reporters files

--- src/laura/reporters/blank/init.lua
+++ src/laura/reporters/blank/init.lua
@@ -1,1 +1,18 @@
-return require("laura.reporters.blank.BlankReporter")
+local Reporter = require("laura.reporters.Reporter")
+
+---@class BlankReporter : Reporter
+local BlankReporter = {}
+
+---@param results RunResults
+---@return RunResults
+function BlankReporter:new(results)
+	setmetatable(results, { __index = BlankReporter })
+	setmetatable(BlankReporter, { __index = Reporter })
+	return results
+end
+
+function BlankReporter:reportTest()
+	-- noop
+end
+
+return BlankReporter
--- src/laura/reporters/dots/init.lua
+++ src/laura/reporters/dots/init.lua
@@ -1,1 +1,36 @@
-return require("laura.reporters.dots.DotsReporter")
+local Reporter = require("laura.reporters.Reporter")
+local Terminal = require("laura.Terminal")
+local Status = require("laura.Status")
+
+local dot = "."
+
+---@class DotsReporter : Reporter
+local DotsReporter = {}
+
+---@param results RunResults
+---@return RunResults
+function DotsReporter:new(results)
+	setmetatable(results, { __index = DotsReporter })
+	setmetatable(DotsReporter, { __index = Reporter })
+	return results
+end
+
+---@private
+---@param status Status
+function DotsReporter:printDot(status)
+	io.write(Terminal.setColor(status) .. dot)
+end
+
+---@param test Runnable
+function DotsReporter:reportTest(test)
+	if test:isSkipped() then
+		self:printDot(Status.Skipped)
+	elseif test:isFailed() then
+		self:printDot(Status.Failed)
+	elseif test:isPassed() then
+		self:printDot(Status.Passed)
+	end
+	io.write(Terminal.reset())
+end
+
+return DotsReporter
